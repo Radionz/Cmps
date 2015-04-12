377278b381664414fc7cf8f86bd97a7942c5b9ee
Dorian Blanc <blanc.dorian.pro@gmail.com>
2015-04-12 09:43:04 +0200

    adding files

--- dbaccess.php
+++ dbaccess.php
@@ -1,3 +1,4 @@
 <?php
-$db = new PDO('pgsql:dbname=postgres;host=127.0.0.1;user=postgres;password=root');
+$db = new PDO('pgsql:dbname=dlk8867oe0a8d;host=ec2-54-163-225-82.compute-1.amazonaws.com;user=yaedbmcycdqwmw;password=Ka-ojSGHcdGx_55g8V7gDoG3Iw');
+//$db = new PDO('pgsql:dbname=postgres;host=127.0.0.1;user=postgres;password=root');
 ?>
